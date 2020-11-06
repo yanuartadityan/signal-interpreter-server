d6bf8cd154d1fd3323748550f0a4927727170ba1
Tri Aditya Nugraha <YTRIADIT@volvocars.com>
2020-11-06 07:49:53 +0100

    added one more route for example

--- signal-interpreter-app/routes.py
+++ signal-interpreter-app/routes.py
@@ -6,3 +6,7 @@ signal_interpreter_app = Flask(__name__)
 @signal_interpreter_app.route("/")
 def hello():
     return "Hello World"
+
+@signal_interpreter_app.route("/member/")
+def ricardo_introduction():
+    return "This is member's page"
